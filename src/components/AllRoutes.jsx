3b9f55991a0eff5d16291f7e7ddf063f1c6d68cf
Brian Henning <henninb@msn.com>
2023-08-19 16:42:32 -0500

    wip

---
+++ src/components/AllRoutes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Landing from "./Landing";
+import HockeyScores from "./HockeyScores";
+import Home from "./Home";
+import ProtectedRoutes from "./ProtectedRoutes";
+
+export default function AllRoutes() {
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/landing" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/nhl" element={<HockeyScores />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
