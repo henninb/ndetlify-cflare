3321b1ddb076649d2b67775cce7e8acf2a16e002
Brian Henning <henninb@msn.com>
2023-08-20 14:31:28 -0500

    wip

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,28 @@
-// import { Route, Redirect } from 'react-router-dom'
+import { createContext, useContext, useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+
+const AuthContext = createContext();
+export const AuthData = () => useContext(AuthContext);
 
 export default function PrivateRoute() {
 
-    let auth = {'token':false}
+    const [user, setUser] = useState({name: "", isAuthenticated: false});
+
+    const login = (userName, passwd) => {
+      return new Promise((resolve, reject) => {
+      if( passwd === "monday1" ) {
+        setUser({name: userName, isAuthenticated: true})
+        resolve("sucess");
+      } else {
+        reject("invalid credentials");
+      }
+      })
+    }
+
+    //let auth = {'token':false}
+    let myLogin = login('henninb', 'monday1');
     return(
-        auth.token ? <Outlet /> : <Navigate to="/login" />
+        // auth.token ? <Outlet /> : <Navigate to="/login" />
+        user.isAuthenticated ? <Outlet /> : <Navigate to="/login" />
     )
 }
