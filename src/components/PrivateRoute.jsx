74acfde77b0f66d7bb8cf3b34adfc52ef8af7535
Brian Henning <henninb@msn.com>
2023-08-21 18:11:19 -0500

    wip

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const AuthContext = createContext();
