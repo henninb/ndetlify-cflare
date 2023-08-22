dae92b6c440a26128963cb07938c24e3ddfdc548
Brian Henning <henninb@msn.com>
2023-08-21 19:27:23 -0500

    wip

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -7,6 +7,7 @@ import Home from "./Home";
 import ProtectedRoutes from "./ProtectedRoutes";
 import About from "./About";
 import Test from "./Test";
+import Logout from "./Logout";
 //import { createContext, useContext } from "react";
 
 export default function AllRoutes() {
@@ -19,6 +20,7 @@ export default function AllRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/about" element={<About />} />
+            <Route path="/logout" element={<Logout />} />
             <Route element={<ProtectedRoutes />}>
               <Route path="/nhl" exact element={<HockeyScores />} />
               <Route path="/test" element={<Test />} />
