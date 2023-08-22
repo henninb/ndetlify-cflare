ba39e838f984235d987d859144f5eed2820fed5f
Brian Henning <henninb@msn.com>
2023-08-22 17:10:14 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,45 @@
 import React, { Component } from "react";
 import AllRoutes from "./components/AllRoutes";
 export default class App extends Component {
+
+
+    constructor(props) {
+    super(props);
+
+
+        // Initialize isAuthenticated from sessionStorage
+    this.state = {
+      isAuthenticated: sessionStorage.getItem('isAuthenticated') === 'true', // Convert the string to a boolean
+    };
+  }
+
+  // Add event listeners when the component mounts
+  componentDidMount() {
+    window.addEventListener('storage', this.handleStorageChange);
+  }
+
+  // Remove event listeners when the component unmounts
+  componentWillUnmount() {
+    window.removeEventListener('storage', this.handleStorageChange);
+  }
+
+  // Handle changes to sessionStorage
+  handleStorageChange = (e) => {
+    if (e.key === 'isAuthenticated') {
+      this.setState({
+        isAuthenticated: e.newValue === 'true',
+      });
+    }
+  };
+
+
   render() {
-    return <div>
+
+    let isAuthenticated = sessionStorage.getItem('isAuthenticated');
+    // const { isAuthenticated } = this.state;
+    console.log(isAuthenticated);
+
+    return (<div>
       <nav className="navbar navbar-expand-lg navbar-dark primary-color">
      <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
@@ -23,16 +60,22 @@ export default class App extends Component {
       <li className="nav-item">
         <a className="nav-link" href="/about">About</a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/login">Login</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/logout">Logout</a>
-      </li>
+
+               {isAuthenticated !== 'true' ? (
+                <li className="nav-item">
+                  <a className="nav-link" href="/login">Login</a>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <a className="nav-link" href="/logout">Logout</a>
+                </li>
+              )}
+
     </ul>
   </div>
 </nav>
       <AllRoutes />
       </div>
+    )
   }
 }
