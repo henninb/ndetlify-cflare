c974e18dc524c824bf9bb2ad1352c2259d5234e6
Brian Henning <henninb@msn.com>
2024-02-07 18:31:37 -0600

    wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import AllRoutes from "./components/AllRoutes";
-import './App.css';
 
 export default class App extends Component {
-
-
     constructor(props) {
     super(props);
-
 
         // Initialize isAuthenticated from sessionStorage
     this.state = {
@@ -41,41 +37,54 @@ export default class App extends Component {
     // const { isAuthenticated } = this.state;
     console.log(isAuthenticated);
 
-    return (<div>
-      <nav className="navbar navbar-expand-lg navbar-dark primary-color">
-     <div className="collapse navbar-collapse" id="navbarNav">
+    return (
+      <div>
+  <nav className="navbar navbar-expand-lg navbar-dark primary-color">
+    {/* Toggle button for mobile view */}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/nhl">NHL</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/landing">Landing</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/test">Test</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/about">About</a>
-      </li>
+        <li className="nav-item active">
+          <a className="nav-link" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/nhl">NHL</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/landing">Landing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/test">Test</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
+        </li>
 
-               {isAuthenticated !== 'true' ? (
-                <li className="nav-item">
-                  <a className="nav-link" href="/login">Login</a>
-                </li>
-              ) : (
-                <li className="nav-item">
-                  <a className="nav-link" href="/logout">Logout</a>
-                </li>
-              )}
-
-    </ul>
-  </div>
-</nav>
-      <AllRoutes />
-      </div>
+        {isAuthenticated !== 'true' ? (
+          <li className="nav-item">
+            <a className="nav-link" href="/login">Login</a>
+          </li>
+        ) : (
+          <li className="nav-item">
+            <a className="nav-link" href="/logout">Logout</a>
+          </li>
+        )}
+      </ul>
+    </div>
+  </nav>
+  <AllRoutes />
+</div>
     )
   }
 }
