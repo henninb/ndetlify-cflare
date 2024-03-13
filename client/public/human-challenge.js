98047fe6b51935bc9b72034dc2879763af0e8df1
Brian Henning <henninb@gmail.com>
2024-03-13 08:05:12 -0500

    wip

---
+++ client/public/human-challenge.js
@@ -0,0 +1,34 @@
+window['_' + window._pxAppId] = {
+	challenge: {
+		context: {
+			headerText: 'Are you a real person?',
+			headerFontSize: '22px',
+			headerColor: "#000",
+			messageText: 'Help us keep your account safe by completing the below action.',
+			messageFontSize: '16px',
+			messageColor: '#404041',
+			messagePadding: '40px',
+			css: ["https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap", "https://use.typekit.net/zqt8qze.css"],
+			messageFont: "Inter",
+		},
+		translation: {
+			"default": {
+				btn: 'PRESS AND HOLD',
+				textSize: 15,
+			},
+		},
+		view: {
+			fillColor: "#CF0A2C",
+			backgroundColor: "#FFFFFF",
+			borderColor: "#CF0A2C",
+			borderRadius: 50,
+			preset: 1,
+			borderWidth: 4,
+			css: ["https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap", "https://use.typekit.net/zqt8qze.css"],
+			textFont: "Fieldwork",
+			textColor: "#CF0A2C",
+			textSize: 10,
+			fontWeight: 'bold',
+		}
+	}
+};
