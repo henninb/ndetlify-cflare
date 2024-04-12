8a9464576075ae48f4e63a94a61d11d31a29bb1c
Brian Henning <henninb@msn.com>
2024-04-12 15:42:15 -0500

    wip

--- run-docker.sh
+++ run-docker.sh
@@ -8,7 +8,7 @@ docker run --name=react-app -h react-app --restart unless-stopped -p 3000:3000 -
 docker rm -f react-app
 
 docker save -o react-app.tar react-app
-scp react-app.tar 192.168.10.224:/home/henninb/react-app.tar
+scp react-app.tar 192.168.10.15:/home/henninb/react-app.tar
 echo docker load -i react-app.tar
 
 exit 0
