ac9c0c2f2ba9bda7140e04926c56c6f1708f610e
Brian Henning <henninb@msn.com>
2024-04-14 19:10:51 -0500

    wip

--- run-docker.sh
+++ run-docker.sh
@@ -11,7 +11,8 @@ export DOCKER_HOST=tcp://192.168.10.15:2375
 docker rm -f react-app
 docker run --name=react-app -h react-app --restart unless-stopped -p 3000:3000 -d react-app
 
-docker run -d --name cloudflared --restart=unless-stopped cloudflare/cloudflared:latest tunnel --no-autoupdate run --token ${TOKEN)
+docker rm -f cloudflared
+docker run -d --name cloudflared --restart=unless-stopped cloudflare/cloudflared:latest tunnel --no-autoupdate run --token ${TOKEN}
 
 # docker save -o react-app.tar react-app
 # scp react-app.tar 192.168.10.15:/home/henninb/react-app.tar
