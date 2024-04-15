5f235101c13b31e0dd5d2bd3dbbf405756f04d4e
Brian Henning <henninb@msn.com>
2024-04-15 16:33:09 -0500

    wip

--- run-gcp.sh
+++ run-gcp.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 
-echo ssh add ~/.ssh/google_compute_engine
 # eval "$(ssh-agent -s)"
+echo ssh add ~/.ssh/google_compute_engine
 
 TOKEN=$(cat token)
 docker context create remote-webserver --docker "host=ssh://brianhenning@35.226.225.26"
 export DOCKER_HOST=ssh://brianhenning@35.226.225.26
+
 npm install
 npm run build
 docker build -t react-app .
@@ -21,8 +22,5 @@ docker ps -a
 
 echo gcloud compute firewall-rules create allow-react-app-rule --allow tcp:3001
 echo gcloud compute ssh --zone "us-central1-b" "owasp-juice-shop-app" --project "sa-brian-henning"
-# docker save -o react-app.tar react-app
-# scp react-app.tar 192.168.10.15:/home/henninb/react-app.tar
-# echo docker load -i react-app.tar
 
 exit 0
