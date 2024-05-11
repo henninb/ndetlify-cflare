3d55e0cd952ba1cad9ec10d9fca7e7dbb432173a
Brian Henning <henninb@msn.com>
2024-05-11 16:25:33 -0500

    wip

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,7 +3,6 @@ import { useMatch } from 'react-router-dom';
 export default function Transactions() {
 
   const routeMatch = useMatch("/transactions/:account");
-  console.log('here');
     return (
       <div>
       Transactions - {routeMatch.params["account"]}
