976507bb23abdb72c44591e956b688d908210d68
Brian Henning <henninb@msn.com>
2024-05-11 15:54:59 -0500

    wip

--- run-docker.sh
+++ run-docker.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 TOKEN=$(cat token)
-docker context create remote-webserver --docker "host=ssh://henninb@192.168.10.15"
+docker context create remote-webserver-ssh --docker "host=ssh://henninb@192.168.10.15"
+docker context create remote-webserver-tcp --docker "host=tcp://192.168.10.15:2375"
 #npm install
 mkdir -p ssl
 npm run build
 docker build -t react-app .
 # docker build -f Dockerfile-nginx -t react-app-nginx .
-docker save react-app | docker --context remote-webserver load
+docker save react-app | docker --context remote-webserver-tcp load
 
-# export DOCKER_HOST=tcp://192.168.10.15:2375
+export DOCKER_HOST=tcp://192.168.10.15:2375
 docker rm -f react-app
 docker run --name=react-app -h react-app --restart unless-stopped -p 3000:3000 -d react-app
+docker ps -a
 
 # docker rm -f cloudflared
 # docker run -d --name cloudflared --restart=unless-stopped cloudflare/cloudflared:latest tunnel --no-autoupdate run --token ${TOKEN}
